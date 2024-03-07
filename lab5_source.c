a20e635d00597beaabbcf62457f1d450ae385a62
Evan <114312843+ehiddema@users.noreply.github.com>
2024-03-07 13:30:13 -0800

    edit

--- lab5_source.c
+++ lab5_source.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <EFM8LB1.h>
+#include <math.h>
 
 // ~C51~  
 
@@ -146,6 +147,64 @@ void waitms (unsigned int ms)
 		for (k=0; k<4; k++) Timer3us(250);
 }
 
+void LCD_byte (unsigned char x)
+{
+	// The accumulator in the C8051Fxxx is bit addressable!
+	ACC=x; //Send high nible
+	LCD_D7=ACC_7;
+	LCD_D6=ACC_6;
+	LCD_D5=ACC_5;
+	LCD_D4=ACC_4;
+	LCD_pulse();
+	Timer3us(40);
+	ACC=x; //Send low nible
+	LCD_D7=ACC_3;
+	LCD_D6=ACC_2;
+	LCD_D5=ACC_1;
+	LCD_D4=ACC_0;
+	LCD_pulse();
+}
+
+void WriteData (unsigned char x)
+{
+	LCD_RS=1;
+	LCD_byte(x);
+	waitms(2);
+}
+
+void WriteCommand (unsigned char x)
+{
+	LCD_RS=0;
+	LCD_byte(x);
+	waitms(5);
+}
+
+void LCD_4BIT (void)
+{
+	LCD_E=0; // Resting state of LCD's enable is zero
+	// LCD_RW=0; // We are only writing to the LCD in this program
+	waitms(20);
+	// First make sure the LCD is in 8-bit mode and then change to 4-bit mode
+	WriteCommand(0x33);
+	WriteCommand(0x33);
+	WriteCommand(0x32); // Change to 4-bit mode
+
+	// Configure the LCD
+	WriteCommand(0x28);
+	WriteCommand(0x0c);
+	WriteCommand(0x01); // Clear screen command (takes some time)
+	waitms(20); // Wait for clear screen command to finsih.
+}
+
+void LCDprint(char * string, unsigned char line, bit clear)
+{
+	int j;
+
+	WriteCommand(line==2?0xc0:0x80);
+	waitms(5);
+	for(j=0; string[j]!=0; j++)	WriteData(string[j]);// Write the message
+	if(clear) for(; j<CHARS_PER_LINE; j++) WriteData(' '); // Clear the rest of the line
+}
 
 void InitPinADC (unsigned char portno, unsigned char pinno)
 {
@@ -195,10 +254,18 @@ void TIMER0_Init(void)
 	TR0=0; // Stop Timer/Counter 0
 }
 
-float get_period()
+unsigned int Get_ADC (void)
+{
+	ADINT = 0;
+	ADBUSY = 1;
+	while (!ADINT); // Wait for conversion to complete
+	return (ADC0);
+}
+
+float get_period_ref()
 {
 	int overflow_count = 0;
-	float half_period;
+	float period;
         
 	while(P2_2!=0); // Wait for the signal to be zero
 	while(P2_2!=1); // Wait for the signal to be one
@@ -220,21 +287,170 @@ float get_period()
 		}
 	}
 	TR0=0; // Stop timer 0, the 24-bit number [overflow_count-TH0-TL0] has the period!
-	half_period = (overflow_count*65536.0+TH0*256.0+TL0)*(12.0/SYSCLK);
-	printf("period: %f\n", half_period); 
+	period = (overflow_count*65536.0+TH0*256.0+TL0)*(12.0/SYSCLK);
+	printf("period: %f\n", period); 
 	TH0 = 0;
 	TL0 = 0;
 	
-	return 2*half_period;
+	return period;
+}
+
+float get_period_oth()
+{
+	int overflow_count = 0;
+	float period;
+	
+        
+	while(P2_4!=0); // Wait for the signal to be zero
+	while(P2_4!=1); // Wait for the signal to be one
+	TH0 = 0;
+	TL0 = 0;
+
+	TR0=1; // Start the timer
+	while(P2_4!=0) // Wait for the signal to be zero
+	{
+		if(TF0==1) // Did the 16-bit timer overflow?
+		{
+			TF0=0;
+			overflow_count++;
+		}
+	}
+	while(P2_4!=1) // Wait for the signal to be one
+	{
+		if(TF0==1) // Did the 16-bit timer overflow?
+		{
+			TF0=0;
+			overflow_count++;
+		}
+	}
+	TR0=0; // Stop timer 0, the 24-bit number [overflow_count-TH0-TL0] has the period!
+	period = (overflow_count*65536.0+TH0*256.0+TL0)*(12.0/SYSCLK);
+	printf("period: %f\n", period); 
+	TH0 = 0;
+	TL0 = 0;
+	
+	return period;
+}
+
+float get_timediff()
+{
+	int overflow_count = 0;
+	float difference;
+        
+	ADC0MX = QFP32_MUX_P2_5;
+	ADINT = 0;
+	ADBUSY = 1;
+	while (!ADINT);
+	while(Get_ADC()!=0); // Wait for the REF signal to be zero
+	while(Get_ADC()==0); // Wait for the REF signal to be one
+	TH0 = 0;
+	TL0 = 0;
+	TR0 = 1; // Start the timer
+
+	ADC0MX = QFP32_MUX_P2_3;
+	ADINT = 0;
+	ADBUSY = 1;
+	while (!ADINT);
+	while(Get_ADC()!=0) // Wait for the OTHER signal to be zero
+	{
+		if(TF0==1) // Did the 16-bit timer overflow?
+		{
+			TF0=0;
+			overflow_count++;
+		}
+	}
+	while(Get_ADC()==0) // Wait for the OTHER signal to be one
+	{
+		if(TF0==1) // Did the 16-bit timer overflow?
+		{
+			TF0=0;
+			overflow_count++;
+		}
+	}
+	TR0 = 0; // Stop timer 0, the 24-bit number [overflow_count-TH0-TL0] has the period!
+	difference = (overflow_count*65536.0+TH0*256.0+TL0)*(12.0/SYSCLK);
+	TH0 = 0;
+	TL0 = 0;
+	
+	return difference;
+}
+float get_peakVolt_ref(float period)
+{
+	float peak_volt = 0.0;
+
+	ADC0MX = QFP32_MUX_P2_3;
+	ADINT = 0;
+	ADBUSY = 1;
+	while (!ADINT);
+	while(peak_volt==0.0){
+		while(Get_ADC()!=0); // Wait for pin to be 0
+		while(Get_ADC()==0); // wait for pin to be 1
+		waitms(((int)(period*1000.0/4.0) ));
+		//printf("(period_ref*1000.0/4.0) = %d", (int)(period*1000.0/4.0));		
+		peak_volt = Volts_at_Pin(QFP32_MUX_P2_3);
+		//printf ("V@P2.3=%7.5fV\n", peak_volt);
+		}
+
+	return peak_volt;
+}
+
+float get_peakVolt_oth(float period)
+{
+	float peak_volt = 0.0;
+
+	ADC0MX = QFP32_MUX_P2_5;
+	ADINT = 0;
+	ADBUSY = 1;
+	while (!ADINT);
+	while(peak_volt==0.0){
+		while(Get_ADC()!=0); // Wait for pin to be 0
+		while(Get_ADC()==0); // wait for pin to be 1
+		waitms(((int)(period*1000.0/4.0) ));
+		//printf("(period_oth*1000.0/4.0) = %d", (int)(period*1000.0/4.0));		
+		peak_volt = Volts_at_Pin(QFP32_MUX_P2_5);
+		//printf ("V@P2.3=%7.5fV\n", peak_volt);
+		}
+
+	return peak_volt;
+}
+
+#include <string.h>
+void display_data(float freq, float vref, float voth, float phase)
+{
+	char buffer[17];
+	char str1[] = "vr%f2.1 vo%f2.1";
+	char str2[] = "f%f2.1 p%f3.1";
+	strcpy(buffer, vref, voth, str1);
+	LCDprint(buffer, 1, 1);
+
+	strcpy(buffer, freq, phase, str2);
+	LCDprint(buffer, 2, 1);
+
 }
 
 
 void main (void)
 {
-	float v[4];
-	float period;
+	// PINOUTS:
+	// P2_2 = digital input of reference signal
+	// P2_3 = analog input of reference signal
+	// P2_4 = digital input of OTHER signal
+	// P2_5 = analog input of OTHER signal
+	
+	char buffer[16];
+	
+	float v[2];
+	float period_ref;
+	float frequency_ref;
+	// float period_oth;
+	// float frequency_oth;
+	float peak_ref_volt;
+	float rms_ref;
+	float peak_oth_volt;
+	float rms_oth;
+	float timediff;
+	float phase;
 
-   	
 	TIMER0_Init(); 
 	
     waitms(500); // Give PuTTy a chance to start before sending
@@ -247,25 +463,45 @@ void main (void)
 	        
 	
 	//InitPinADC(2, 2); // Configure P2.2 as analog input
-	//InitPinADC(2, 3); // Configure P2.3 as analog input
-	InitPinADC(2, 4); // Configure P2.4 as analog input
+	InitPinADC(2, 3); // Configure P2.3 as analog input
+	//InitPinADC(2, 4); // Configure P2.4 as analog input
 	InitPinADC(2, 5); // Configure P2.5 as analog input
     InitADC();
 
         
 	while(1)
 	{
-        
-        
+		period_ref = get_period_ref(); // this period is in seconds
+		waitms(500);
 
-		period = get_period();
+		// convert period to frequency and display
+		frequency_ref = 1.0/period_ref;
+		waitms(500);
+
+		// wait for zero-cross, then wait for period/4 and measure the peak voltage of the reference signal -------------------------------------------  
+		peak_ref_volt = get_peakVolt_ref(period_ref);
+		waitms(500);
+
+		// wait for zero-cross, then wait for period/4 and measure the peak voltage of the OTHER signal -------------------------------------------
+		peak_oth_volt = get_peakVolt_oth(period_ref);
+		waitms(500);
+		// measure the time diff between the zero-cross of both signals -------------------------------------------
+		timediff = get_timediff();
+		printf("timediff = %f",timediff);
+
+		// convert peak ADC values to rms and display them -------------------------------------------
+		rms_ref = peak_ref_volt / 1.4142;
+		rms_oth = peak_oth_volt / 1.4142;
+
+		// convert time diff between zero-crosses to degrees and display -------------------------------------------
+		phase = (360*timediff)/period_ref;
+		printf("phase: %f", phase);
+
 		
 	    // Read 14-bit value from the pins configured as analog inputs
-		v[0] = Volts_at_Pin(QFP32_MUX_P2_2);
-		v[1] = Volts_at_Pin(QFP32_MUX_P2_3);
-		v[2] = Volts_at_Pin(QFP32_MUX_P2_4);
-		v[3] = Volts_at_Pin(QFP32_MUX_P2_5);
-		//printf ("V@P2.2=%7.5fV, V@P2.3=%7.5fV, V@P2.4=%7.5fV, V@P2.5=%7.5fV\r", v[0], v[1], v[2], v[3]);
+		v[0] = Volts_at_Pin(QFP32_MUX_P2_3);
+		v[1] = Volts_at_Pin(QFP32_MUX_P2_5);
+		//printf ("V@P2.3=%7.5fV, V@P2.5=%7.5fV\r", v[0], v[1]);
 		waitms(500);
 	}
 }	
